d8589b5ea7adb5972d0549ffcc033442ba061ec6
Jonny Reading <jonny@firefinch.io>
2023-07-08 21:58:12 +0100

    dummy about and contact pages

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AboutPage = () => {
+
+  return (
+    <>
+      <div className="text-container">
+        <h2>About me.</h2>
+      </div>
+      <p className="padded-text">
+        So who am I? I'm an enigma, a shadow, the fleeting glimpse of a squirrel running up a tree.
+      </p>
+    </>
+  );
+}
+
+export default AboutPage;
---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactPage = () => {
+
+  return (
+    <div className='padded-text'>
+      contact me pls.
+    </div>
+  );
+}
+
+export default ContactPage;
