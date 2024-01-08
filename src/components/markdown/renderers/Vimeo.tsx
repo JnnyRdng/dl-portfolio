dc5bfa9b79fcfbb8f82ac59cc54bd501b45fbd17
Jonny <jonnyreading@gmail.com>
2024-01-08 00:53:53 +0000

    allow private vimeo videos adds hash param to iframe src

--- src/components/markdown/renderers/Vimeo.tsx
+++ src/components/markdown/renderers/Vimeo.tsx
@@ -3,16 +3,17 @@ import styles from './VideoEmbed.module.scss';
 
 interface Props {
   videoid: string;
+  h?: string;
   width?: number;
   height?: number;
 }
 
-export const Vimeo = ({ videoid }: Props) => {
-
+export const Vimeo = ({ videoid, h }: Props) => {
+  const hash = h ? `h=${h}&` : '';
   return (
     <>
       <div className={styles.videoResponsive}>
-        <iframe src={`https://player.vimeo.com/video/${videoid}?byline=0&title=0`} width="100%" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+        <iframe src={`https://player.vimeo.com/video/${videoid}?${hash}byline=0&title=0`} width="100%" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
       </div>
     </>
   );
@@ -22,6 +23,7 @@ const isVimeoProps = (data: any): data is Props => {
   return (
     typeof data === 'object' &&
     typeof data.videoid === 'string' &&
+    (data.h === undefined || typeof data.h === 'string') &&
     (typeof data.width === 'undefined' || (typeof data.width === 'string' && !isNaN(parseInt(data.width, 10)))) &&
     (typeof data.height === 'undefined' || (typeof data.height === 'string' && !isNaN(parseInt(data.height, 10))))
   );
@@ -29,5 +31,5 @@ const isVimeoProps = (data: any): data is Props => {
 
 export const markdownToVimeo = (data: any) => {
   if (!isVimeoProps(data)) return null;
-  return <Vimeo videoid={data.videoid} />
+  return <Vimeo videoid={data.videoid} h={data.h} />
 }
